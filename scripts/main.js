8f292c19f4734ad0f444d4ccb4a4c32b789d8805
CarlLight67 <carlight67@users.noreply.github.com>
2026-06-29 18:16:52 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// 1. YOUR MINI-DATABASE (Add all 15 items here)
+const products = [
+    { id: 1, name: "Piattos", price: "₱15.00", image: "../images/piatos.png" },
+    { id: 2, name: "Nova", price: "₱15.00", image: "../images/nova.png" },
+    { id: 3, name: "Chippy", price: "₱12.00", image: "../images/chippy.png" },
+    { id: 4, name: "Mr. Chips", price: "₱15.00", image: "../images/mrchips.png" },
+    { id: 5, name: "Oishi", price: "₱10.00", image: "../images/oishi.png" },
+    // ... you can easily add items 6 through 15 right here!
+];
+
+// 2. TARGET THE EMPTY CONTAINER
+const productContainer = document.getElementById ('container')
